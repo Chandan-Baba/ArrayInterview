c7f93df2e066541254fd026bf650125e4b319c73
Chandan <47283424+Chandan-Baba@users.noreply.github.com>
2022-11-04 17:32:03 +0530

    Update sum of nums in array.js

--- sum of nums in array.js
+++ sum of nums in array.js
@@ -12,3 +12,20 @@ function sumOFArray(arrayOfNums)
 
 
 console.log(sumOFArray(arrayOfNums))
+
+
+function sumArray(ar)
+{
+    let sum = 0;
+    
+    for(let i = 0; i < ar.length; i++)
+    {
+        sum += ar[i];
+    }
+    
+    return sum;
+}
+
+let ar = [2, 3, -1, 5, 7, 9, 10, 15, 95];
+let sum = sumArray(ar);
+console.log(sum);
